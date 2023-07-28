32a9db9bf8299ec1455ee72e0c96e4d03a220e46
Anik <66571431+NavinAnik@users.noreply.github.com>
2023-07-28 18:59:09 +0600

    Time: 50 ms (90.00%) | Memory: 41.7 MB (63.33%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+    let ans = '';
+
+    while(columnNumber>0){
+        let code = (--columnNumber)%26;
+        ans = String.fromCharCode(code+65) + ans;
+        columnNumber = (columnNumber-code)/26;
+    }
+    
+    return ans;
+};
