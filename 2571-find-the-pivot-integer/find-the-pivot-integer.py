8afa9a87daa62159d53a3b674232ffbd6f1bc417
Anik <66571431+NavinAnik@users.noreply.github.com>
2024-03-13 17:13:47 +0600

    Time: 12 ms (90.85%) | Memory: 11.7 MB (56.21%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def pivotInteger(self, n):
+        x = sqrt(n * (n + 1) / 2)
+        
+        if x % 1 != 0:
+            return -1
+        else:
+            return int(x)
+        
